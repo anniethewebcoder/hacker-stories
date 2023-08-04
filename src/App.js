13dde9d3a5e6e40e98c13d05623f9142ea478b2c
anniethewebcoder <annie.the.web.coder@gmail.com>
2023-08-04 11:03:52 -0400

    practice

--- src/App.js
+++ src/App.js
@@ -1,89 +1,153 @@
 import './App.css';
 import * as React from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-// const welcome = {
-//   greeting: 'Hey',
-//   title: 'React',
-// };
-
-// function getTitle(title) {
-//   return title;
-// }
-
-const list = [
+const initialStories = [
   {
     title: 'React',
-    url: 'https://reactjs.org/',
+    url: 'https://reactjs.org',
     author: 'Jordan Walke',
     num_comments: 3,
-    points: 4, 
+    points: 4,
     objectID: 0,
   },
   {
     title: 'Redux',
-    url: 'https://redus.js.org',
-    author: 'Dan Abramov, Andrew Clerk',
-    num_comments: 2,
-    points: 5,
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2, 
+    points: 5, 
     objectID: 1,
-  },
+  }
 ];
 
-// defining App
+const useSemiPersistentState = (key, initialState) => {
+  const [value, setValue] = useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  useEffect(() => {
+    localStorage.setItem('value', value);
+  }, [value, key]);
+
+  return [value, setValue];
+}
+
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useSemiPersistentState(
+    'search',
+    'React'
+  );
+
+  const [stories, setStories] = useState(initialStories);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  }
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+
+    localStorage.setItem('search', event.target.value);
+  }
   
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-      {/* <h1>
-        {welcome.greeting} {welcome.title}
-      </h1> */}
-
-      {/* <h1>Hello {getTitle('React')}</h1> */}
-
       <h1>My Hacker Stories</h1>
 
-      {/* creating an instance of Search component */}
-      <Search />
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
 
-      {/* creating an instance of List component */}
-      <List />
-
-      {/* creating another instance of List component */}
-      <List />
-      
+      <List list={searchedStories}/>
     </div>
   );
 }
 
-const Search = () => {
+const List = ({ list, onRemoveItem }) => (
+  <ul>
+    {list.map(({ objectID, ...item}) => (
+      <Item 
+        key={objectID} 
+        item={item}
+        onRemoveItem={onRemoveItem}
+      />
+    ))}
+  </ul>
+);
+
+const Item = ({ 
+    item,
+    onRemoveItem
+}) => {
+  const handleRemoveItem = () => {
+    onRemoveItem(item);
+  };
+
   return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-    </div>
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={handleRemoveItem}>
+          Dismiss
+        </button>
+      </span>
+    </li>
+  );
+};
+
+const InputWithLabel = ({ 
+  id,
+  value,
+  type="text", 
+  onInputChange,
+  isFocused,
+  children
+}) => {
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if(isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return(
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
   );
 }
 
-//definition of List component
-const List = () => {
-  return (
-    <ul>
-      {list.map((item) => {
-        return (
-          <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
 
 export default App;
